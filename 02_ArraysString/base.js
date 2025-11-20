ab22f7d9a3bab1525d34b1e0878bec4ba0e796bc
Maksym <pilipm449@gmail.com>
2025-11-20 21:22:10 +0200

    Part 2 done HW

--- 02_ArraysString/base.js
+++ 02_ArraysString/base.js
@@ -70,7 +70,7 @@ console.log("Task 7")
 let similarNums = []
 
 for (let i = 0; i < arr.length; i++) {
-    for (let u = i+1; u < arr.length; u++) {
+    for (let u = i + 1; u < arr.length; u++) {
         if (arr[i] == arr[u]) { similarNums.push(u); break; }
     }
 }
@@ -84,7 +84,7 @@ console.log("Task 8")
 let newArr = []
 let counter = 0;
 
-for (let i = 1; i < arr.length-1; i++) {
+for (let i = 1; i < arr.length - 1; i++) {
     newArr[counter] = arr[i];
     counter++;
 }
@@ -97,8 +97,107 @@ console.log("Task 9")
 let counter2 = 0;
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i]%2==0) {counter2++}
+    if (arr[i] % 2 == 0) { counter2++ }
 }
-console.log(`Number of even numbers: ${counter2}`)
+console.log(`Number of even numbers: ${counter2}`);
+
+
+//-----------------------------------------
+console.log("Part 2 Task 1")
+
+// Завдання 1:
+// Написати функцію, яка приймає рядок та рахує кількість пробілів в ньому.
+
+let str = prompt("Enter String")
+let spaceCounter = 0;
+
+for (let i = 0; i < str.length; i++) {
+    if (str[i] == " ") { spaceCounter++ };
+}
+console.log(`Number Of Spaces: ${spaceCounter}`);
+    
+
+// Завдання 2:
+// Написати функцію, яка переводить перший сиивол рядка у верхній регістр.
+
+console.log("Part 2 Task 2")
+
+let str2 = prompt("Enter string");
+
+let words2 = str2.split(" ");
+
+for (let i = 0; i < words2.length; i++) {
+    words2[i] = words2[i][0].toUpperCase() + words2[i].slice(1);
+}
+
+console.log(words2)
+
+// Завдання 3:
+// Написати функцію, яка рахує кількість слів в рядку.
+
+console.log("Part 2 Task 3");
+
+let str3 = prompt("Enter string");
+let words3 = str2.split(" ");
+
+let wordCounter = words3.length;
+console.log(`Number of words in [${str3}] -- ${wordCounter}`);
+
+
+// Завдання 4:
+// Написати функцію, яка приймає словосполучення і перетворює його в абревіатуру.
+// Наприклад: cascading style sheets в CSS, об'єктно орієнтоване програмування в ООП.
+
+console.log("Part 2 Task 4")
+
+let str4 = prompt("Enter words for Abriviation")
+let words4 = str4.split(" ")
+
+let abbr = "";
+
+for (let i = 0; i < words4.length; i++) {
+    abbr += words4[i][0].toUpperCase(); 
+}
+
+console.log(`Abriviation for [${str4}] - ${abbr}`);
+
+
+// Завдання 5:
+// Написати функцію, яка перевіряє чи переданий рядок являється паліндромом.
+
+console.log("Part 2 Task 5");
+
+let palindrom = prompt("ENter a word to find if it is palindrom")
+palindrom = palindrom.trim();
+
+if (palindrom[0] == palindrom[palindrom.length - 1]) { console.log(`Word ${palindrom} -- is palindrom`) }
+else { console.log(`Word ${palindrom} -- is not palindrom`) }
+
+
+// *Завдання 6:
+// Написати функцію, яка отримує url та відображає детальну інформацію про неї.
+// Наприклад: url “https://itstep.org/ua/about”, інформація “протокол: https, домен:
+// itstep.org, шлях: /ua/about”.
+
+console.log("Part 2 Task 5");
+
+let url = prompt("Enter URL");
+url = url.trim();
+
+let protocol = url.split(":");
+let noProtocoldomenPath = protocol[1].split("//"); 
+
+let domenAndPath = noProtocoldomenPath[1].split("/");
+let domen = domenAndPath[0];
+
+let path = "/";
+
+for (let i = 1; i < domenAndPath.length; i++) {
+    path += domenAndPath[i].concat("/");
+}
+
+console.log(`protocol: [ ${protocol[0]} ], domen: [ ${domen} ], path: [ ${path} ]`)
+
+
 
 
