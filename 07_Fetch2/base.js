636d12f9fc6cc6d17057471faeca2789b7ef3ae9
Maksym <pilipm449@gmail.com>
2025-12-02 23:52:38 +0200

    Tasks 1-2 done HW

--- 07_Fetch2/base.js
+++ 07_Fetch2/base.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     imgDog.hidden = true;
 
     fetchImage();
+    fetchDogBreeds();
 })
 
 function fetchImage() {
@@ -16,10 +17,50 @@ function fetchImage() {
         .then(data => {
             imgDog.src = data.message;
         });
-
-    // let response = await fetch(url);
-    // let json = await response.json();
-
-    // imgDog.src = json.message;
-    // console.log(json);
 };
+
+
+function fetchDogBreeds() {
+    //const url = "https://dog.ceo/api/breed/{airedale}/images/random"
+    const url = "https://dog.ceo/api/breeds/list/all"
+
+    const table = document.getElementById('breedTable')
+
+    fetch(url).then(result => result.json()).then(data => {
+        for (const breed of Object.keys(data.message)) {
+            table.innerHTML += `<tr>
+                        <th scope="row">${breed}</th>
+                        <td scope="row">${data.message[breed]}</td>
+                     </tr>`
+        }
+    }).catch(error => console.error(error));
+}
+
+
+function getText() {
+    const text = document.getElementById('inputTip');
+    console.log(text.value);
+
+    fetchAdviceByKeyword(text.value)
+}
+
+function fetchAdviceByKeyword(keyword) {
+    //let word = prompt("Enter Word For Advice");
+    const url = `https://api.adviceslip.com/advice/search/${keyword}`;
+
+
+    fetch(url).then(response => response.json()).then(data => {
+        const tipBox = document.getElementById('tipBox');
+
+        if (data.slips) {
+            tipBox.innerText = `${data.slips[0].advice} [${data.slips[0].date}]`;
+        }
+        else { tipBox.innerText = data.message.text };
+        console.log(data.message.text)
+
+    }).catch(error => console.error(error));
+
+
+}
+
+
