4b4967d088fa896f52a4118bfec0e8b24214a2a3
Maksym <pilipm449@gmail.com>
2025-12-07 20:23:09 +0200

    Tasks 1-3 done HW

--- 09_BOMDOM/base.js
+++ 09_BOMDOM/base.js
@@ -17,7 +17,7 @@ function decreaseNum(){
 
 let currentImgIndex = 1
 let images = document.getElementById('libraryImg')
-let imgArr = ['https://static.wikia.nocookie.net/villains/images/7/7b/Tylerbetterpicture.jpg','https://i.pinimg.com/736x/f2/bf/03/f2bf03e90b992ee844f6dd0f32b82546.jpg','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShOfsGSXf46P0QLD8faduGNKkNJ_ozF4gqVQ&s','https://ichef.bbci.co.uk/images/ic/480xn/p07h2zhs.jpg.webp','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-0iVNsF5jEfVfGFqGaa6lDvB5D77UIr-GMQ&s','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWQTOH0Xi7a-w9sYtyAW7LcjBY1GX-jSjtaw&s']
+let imgArr = ['https://static.wikia.nocookie.net/villains/images/7/7b/Tylerbetterpicture.jpg','https://i.pinimg.com/736x/f2/bf/03/f2bf03e90b992ee844f6dd0f32b82546.jpg','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShOfsGSXf46P0QLD8faduGNKkNJ_ozF4gqVQ&s','https://ichef.bbci.co.uk/images/ic/480xn/p07h2zhs.jpg.webp','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-0iVNsF5jEfVfGFqGaa6lDvB5D77UIr-GMQ&s','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWQTOH0Xi7a-w9sYtyAW7LcjBY1GX-jSjtaw&s','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_v2u83oA1QyLtDn2Y1G_5aR4BngAxRUgC8w&s','https://yt3.googleusercontent.com/ytc/AIdro_mxX8Srq2DVw-asfvgs0cXVyOOWUgXskAXn7otWaG-cdA=s900-c-k-c0x00ffffff-no-rj']
 
 function leftImg(){
     if(currentImgIndex === 0){
@@ -38,9 +38,11 @@ function rigthImg(){
     }
 }
 
+const textWithColor = document.getElementById("textToChangeColor")
 function changeColorByClick(event){
-    let color = event.target;
-    console.log(color)
+    let box = event.target;
+    let color = box.style.backgroundColor;
+    textWithColor.style.color = color;
 }
 
 
